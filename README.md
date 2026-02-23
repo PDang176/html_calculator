2c78af40924bda89f1748067e9fee32e9227cdb0
Patrick Dang <patrickdangt@gmail.com>
2026-02-22 17:11:11 -0800

    SHUNTING_YARD: Prep for implementing shunting_yard algorithm over using eval function

--- README.md
+++ README.md
@@ -11,6 +11,7 @@ Simple web based calculator made with just HTML/CSS/JS
     3. Multiplication (*)
     4. Division (/)
 3. Capable of Following Standard Order of Operations (PEMDAS)
+    1. Uses the Shunting Yard Algorithm
 4. Registers solving equation only after equals (=) button has been selected
 5. Clear All Button
 6. Backspace Button
@@ -18,7 +19,8 @@ Simple web based calculator made with just HTML/CSS/JS
 ## Optional Features
 
 1. Floating Point Values (decimal button .)
-2. Exponential Operation
-3. Parentheses Grouping
-4. Ans Button to Reference Last Answer
-5. Keyboard Inputs
+2. Plus-Minus Button
+3. Exponential Operation
+4. Parentheses Grouping
+5. Ans Button to Reference Last Answer
+6. Keyboard Inputs
--- js/main.js
+++ js/main.js
@@ -11,21 +11,32 @@ const currentDisplay = display.querySelector("#current");
 const handleButtonClick = (event) => {
   const symbol = event.target.textContent;
   currentDisplay.textContent += symbol;
+
+  equalsButton.disabled = false;
 };
 
 const handleClearClick = () => {
   currentDisplay.textContent = currentDisplay.textContent.slice(0, -1);
+
+  if (currentDisplay.textContent.length === 0) {
+    equalsButton.disabled = true;
+  }
 };
 
 const handleClearAllClick = () => {
   currentDisplay.textContent = "";
+
+  equalsButton.disabled = true;
 };
 
 const handleEqualsClick = () => {
   try {
     const expression = currentDisplay.textContent;
-    const result = eval(expression.replaceAll("x", "*"));
     prevDisplay.textContent = expression + "=";
+
+    const regex = /([a-zA-Z_]\w*(?=\s*\()|\d+\.\d+|\d+|[\+\-\*\/\(\)\^])/g;
+    const tokens = expression.match(regex);
+
     currentDisplay.textContent = result;
   } catch (error) {
     currentDisplay.textContent = "Error";
